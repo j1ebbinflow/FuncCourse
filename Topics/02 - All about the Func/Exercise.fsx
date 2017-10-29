85869099d80cf2a27c5e8f39c06326265dfe4890
Jason Ebbin <jason.m.ebbin@gmail.com>
2017-10-29 16:03:14 +0000

    Complete exercise file for 02

--- Topics/02 - All about the Func/Exercise.fsx
+++ Topics/02 - All about the Func/Exercise.fsx
@@ -7,12 +7,19 @@ let panagram' =
     |> List.map (string >> test.Contains) 
     |> List.reduce (&&) 
 
-
+(*
+    Above is an implementation of the test for a panagram that uses higher order functions and composition.
+    Reimplement the panagram test using recusion, in a way that does a single pass over the inputString. 
+    If possible, avoid using higher order Collection functions (E.g. List.map)
+    Note: this exercise has been pulled from http://exercism.io/exercises/fsharp/pangram/readme
+*)
 let rec panagramRec inputString = true
 
+//Implement a function that recursively sums a list of functions. 
 let rec sumList list = 0
 
 // Reimplementing standard library functions
+// NOTE: The answers for these functions can be easily found online, so don't search for them until you have given it a try and you've reached trouble. 
 
 //TODO Reimplement List.map -> https://msdn.microsoft.com/en-us/library/ee370378.aspx
 // Map takes a function and a list, applies the function to each element in the list, and returns a new list of the results. 
@@ -28,4 +35,23 @@ let rec foldList folder acc list = []
 //Throws an ArgumentException if the list is empty (Use invalidArg function -> https://msdn.microsoft.com/en-us/library/dd233178.aspx ) 
 let rec reduceList reducer list = []
 
-//Using 
+//Using recursion, group an input list of numbers by the final digit. 
+// Hint: To make things easier, you may want to use an array as apart of the output, but using a list or array is acceptable. 
+let rec groupByFinalDigit list = [||]
+
+// Using any approach (Recursion, Function composition or higher order functions), create a function that returns the squares of the positive numbers in a list
+let squares = []
+
+// Using any approach (Recursion, Function composition or higher order functions), create a function that returns the doubles (*2) of the positive numbers in a list
+let doubles = []
+
+(*
+    Using the functions above, create a function that takes in an input list of numbers and does the following
+    - Groups the numbers by the final digit
+    - For each group, if the final digit of all the numbers are even, the numbers are squared. If the final digit of all the numbers are odd, the numbers are doubled
+    - For each group, the numbers are summed 
+    - The square root of each sum is taken
+    - The largest and smallest square root is returned, with the digit of the original group the square root came from. 
+*)
+let doABunchOfMathsStuff = []
+   
